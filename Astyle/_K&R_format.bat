5af55ffbc752bde1a3041da26f7fac08398330c9
Cat <843553493@qq.com>
2018-05-11 14:30:30 +0800

    update
    update

--- Astyle/_K&R_format.bat
+++ Astyle/_K&R_format.bat
@@ -1,9 +1,8 @@
-REM 批量将本目录中的所有C++文件用Astyle进行代码美化操作
-REM 设置Astyle命令位置和参数
+REM using Astyle to format all .c .cpp .h .hpp files recursively start from this directory
 @echo off
-REM 循环遍历目录
+REM format files recursively
 for /r . %%f in (*.cpp;*.c) do astyle --style=kr -S -Z -xW -f -p "%%f"
 for /r . %%f in (*.hpp;*.h) do astyle --style=kr -S -Z -xW -f -p "%%f"
-REM 删除所有的astyle生成文件
+REM delete all temp. files
 for /r . %%f in (*.orig) do del "%%f"
 pause
