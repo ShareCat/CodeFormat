599dd7c802ac61cf90a9a85a3cae5c254e589459
Cat <843553493@qq.com>
2018-05-18 14:14:57 +0800

    add " -U -c"
    "-U": Remove extra space padding around parenthesis on the inside and outside
    "-c": Converts tabs into spaces in the non-indentation part of the line

--- Astyle/_K&R_format.bat
+++ Astyle/_K&R_format.bat
@@ -1,8 +1,8 @@
 REM using Astyle to format all .c .cpp .h .hpp files recursively start from this directory
 @echo off
 REM format files recursively
-for /r . %%f in (*.cpp;*.c) do astyle --style=kr -S -Z -xW -f -p "%%f"
-for /r . %%f in (*.hpp;*.h) do astyle --style=kr -S -Z -xW -f -p "%%f"
+for /r . %%f in (*.cpp;*.c) do astyle --style=kr -S -Z -xW -f -p -U -c "%%f"
+for /r . %%f in (*.hpp;*.h) do astyle --style=kr -S -Z -xW -f -p -U -c "%%f"
 REM delete all temp. files
 for /r . %%f in (*.orig) do del "%%f"
 pause
